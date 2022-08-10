14f62401873da9d5915868f6904b387a7919e219
Gabriel Aparecido de Almeida <gabriel.almeida@novamerica.com.br>
2022-08-10 17:00:17 -0300

    1

--- Artigo.php
+++ Artigo.php
@@ -1,9 +1,15 @@
 <?php 
 
+/* A finalidade desse arquivo que abriga a classe Artigos é de escrever o código PHP pe irá automatizar
+a donstrução e edição de posts na pagina do blog e fazer a conexão com banco de dados
+*/
+
 class Artigo
 {
+    // Inicia a variável $mysql
     private $mysql;   
 
+    // Cria a função construtora
     public function __construct(mysqli $mysql)
     {
         $this-> mysql = $mysql;
@@ -11,7 +17,11 @@ class Artigo
 
     public function exibirTodos(): array
     {
+
+    // Realiza consulta no banco de dados
      $resultado =   $this-> mysql->query('SELECT id,titulo,conteudo FROM artigos');
+     
+     // O método fetch_all retorna um array associativo com valor inteiro
      $artigos  = $resultado -> fetch_all(MYSQLI_ASSOC);
 
 
--- index.php
+++ index.php
@@ -1,8 +1,27 @@
 <?php
+
+/* ------------------------------------ Separando os códigos PHP do HTML --------------------------------------- */
+
+/*É boa prática de programação manter os códigos em arquivos separados, para facilitar a edição de códigos e também
+evitar possíveis erros possam danificar a aplicação.
+
+Nesse exercício isolamos os códigos PHP dos artigos em uma classe e encapsulamos esse código em uma função,
+para acessar essa classe e a função criada é necessário serguir os procedimentos a seguir:*/
+
+// Fazer o link com o código feito na classe Artigos, utilixando o require.
+
+
 require_once ('./Artigo.php');
+
+// Instanciando a conexão, feita no arquivo config.php
 require_once('./config.php');
 
+
+// Instanciar a classe Artigos, criando a classe e salvando ele em uma variável, nesse caso usamos $artigo
 $artigo = new Artigo($mysql);
+
+/* Referenciar a função exibirArtigos(), salvando a função dentro da variável $artigos,
+que recebeu o valor da classe Artigos salvo na variável $artigo */
 $artigos = $artigo -> exibirTodos();
 ?>
 <!DOCTYPE html>
